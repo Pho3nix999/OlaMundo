084357ea25513a75f81b04d6511607cf35513784
Pho3nix999 <100255377+Pho3nix999@users.noreply.github.com>
2022-03-09 21:18:21 -0300

    Add files via upload

---
+++ OlaMundo/OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class OlaMundo {
+
+
+    public static void main(String[] args) {
+       
+        System.out.println("ola mundo");
+        
+       
+    }
+    
+}
